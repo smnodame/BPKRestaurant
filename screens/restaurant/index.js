6fe812e86cfd551c4221b1ba30e79d276446c073
boonprakit <boonprakit@proteus-tech.com>
2017-11-21 01:11:30 +0700

    can show drawer in restaurant component

--- screens/restaurant/index.js
+++ screens/restaurant/index.js
@@ -5,18 +5,125 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform, Image, Dimensions } from 'react-native';
 import GridView from 'react-native-super-grid';
-import { Container, Header, Content, Card, CardItem, Text, Body, Left, Button, Icon, Title } from 'native-base';
+import {
+	Content,
+	Text,
+	List,
+	ListItem,
+	Icon,
+	Container,
+	Left,
+	Right,
+	Badge,
+	Button,
+	StyleProvider,
+	getTheme,
+	variables,
+    Header,
+	Item,
+	Input,
+	Body,
+	Title,
+	Tab,
+	Tabs,
+	TabHeading
+} from "native-base"
+import Drawer from 'react-native-drawer'
+import { NavigationActions } from 'react-navigation'
+
+const deviceHeight = Dimensions.get("window").height
+const deviceWidth = Dimensions.get("window").width
+
 
 export default class Restaurant extends Component<{}> {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+		this.openControlPanel = this.openControlPanel.bind(this)
+        this.backToPrograms = this.backToPrograms.bind(this)
+        this.logout = this.logout.bind(this)
+    }
+
+    logout = () => {
+        const resetAction = NavigationActions.reset({
+        	index: 0,
+        	actions: [
+        		NavigationActions.navigate({ routeName: 'Login'})
+        	]
+        })
+        this.props.navigation.dispatch(resetAction)
+	}
+
+    openControlPanel = () => {
+		this._drawer.open()
+	}
+
+    backToPrograms = () => {
+        const resetAction = NavigationActions.reset({
+                index: 0,
+                actions: [
+                    NavigationActions.navigate({ routeName: 'Programs'})
+                ]
+            })
+        this.props.navigation.dispatch(resetAction)
+    }
+
     render() {
 
       return (
-            <Container>
+          <Drawer
+  	        ref={(ref) => this._drawer = ref}
+  	        type="overlay"
+  	        tapToClose={true}
+  	        openDrawerOffset={0.2} // 20% gap on the right side of drawer
+  	        panCloseMask={0.2}
+  	        closedDrawerOffset={-3}
+  	        styles={drawerStyles}
+  	        tweenHandler={(ratio) => ({
+  	            main: { opacity:(2-ratio)/2, backgroundColor: 'black' }
+  	        })}
+  	        content=
+  	        {
+  	            <Container>
+  					<Content bounces={false} style={{ flex: 1, backgroundColor: "#fff", top: -1 }}>
+  						<View style={styles.drawerCover}>
+  							<Image square style={styles.drawerImage} source={  require('../../Images/logo.png') } />
+  						</View>
+  						<List>
+  							<ListItem itemHeader first style={{ paddingBottom: 3 }}>
+  								<Text>ACTIVITIES</Text>
+  							</ListItem>
+  							<ListItem button noBorder onPress={() => this.backToPrograms() }>
+  								<Left>
+  									<Icon active name='home' style={{ color: "#777", fontSize: 26, width: 30 }} />
+  									<Text style={styles.text}>
+  										เลือกโปรเเกรม
+  									</Text>
+  								</Left>
+  							</ListItem>
+  							<ListItem itemHeader first style={{ paddingBottom: 3 }}>
+  								<Text>ACCOUNT</Text>
+  							</ListItem>
+  							<ListItem button noBorder onPress={() => this.logout()}>
+  								<Left>
+  									<Icon active name='log-out' style={{ color: "#777", fontSize: 26, width: 30 }} />
+  									<Text style={styles.text}>
+  										Log Out
+  									</Text>
+  								</Left>
+  							</ListItem>
+  						</ List>
+  					</Content>
+  				</Container>
+  	        }
+  	    >
+            <Container style={{ backgroundColor: '#f4f4f4' }}>
                 <Header style={{ backgroundColor: '#3b5998' }}>
                     <Left>
-                        <Button transparent>
+                        <Button transparent onPress={() => this.openControlPanel()}>
                             <Icon name="md-menu" />
                         </Button>
                     </Left>
@@ -47,8 +154,14 @@ export default class Restaurant extends Component<{}> {
                     </Button>
                 </Content>
             </Container>
+        </Drawer>
       );
   }
+}
+
+const drawerStyles = {
+     drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+     main: {paddingLeft: 3},
 }
 
 const styles = StyleSheet.create({
@@ -72,4 +185,28 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#fff',
   },
+  drawerCover: {
+      alignSelf: "stretch",
+      // resizeMode: 'cover',
+      height: deviceHeight / 3.5,
+      width: null,
+      position: "relative",
+      marginBottom: 10,
+      backgroundColor: '#004B85'
+  },
+  drawerImage: {
+      position: "absolute",
+      // left: (Platform.OS === 'android') ? 30 : 40,
+      left: Platform.OS === "android" ? deviceWidth / 15 : deviceWidth / 12,
+      // top: (Platform.OS === 'android') ? 45 : 55,
+      top: Platform.OS === "android" ? deviceHeight / 13 : deviceHeight / 12,
+      width: 250,
+      height: 75,
+      resizeMode: "cover"
+  },
+  text: {
+      fontWeight: Platform.OS === "ios" ? "500" : "400",
+      fontSize: 16,
+      marginLeft: 20
+  }
 });
